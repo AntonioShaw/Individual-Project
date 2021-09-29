4ccd56c18bf200c5a0313a2f250971fd40e00045
AntonioShaw <tonyshaw0613@gmail.com>
2021-09-29 09:42:44 -0400

    Added login component and deleted unnecessary src files.

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,8 +1,17 @@
 import React from "react";
 
+import { Container } from 'react-bootstrap'
+
 const AUTH_URL =
-    "https://accounts.spotify.com/authorize?client_id=&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+    "https://accounts.spotify.com/authorize?client_id=b3c4726b31fa49f3a940c64feb001e89&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 export default function Login() {
-    return <div></div>;
+    return (
+        <Container>
+            <a className="btn btn-success btn-lg" href={AUTH_URL}>
+                Login With Spotify
+            </a>
+
+        </Container>
+    )
 }
